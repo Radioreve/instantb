26b33a1c4ddf4e5c2eb1b7d9e50aa3ac12de646f
Léo Jacquemin <ljacquemin@octo.com>
2017-08-21 21:18:34 +0200

    reprise rue du fouré

--- js/app.js
+++ js/app.js
@@ -1,64 +1,62 @@
-
 /*
-	Smooth scrolling
-	When clicking on a anchor (<a href="...">)
-*/
+ Smooth scrolling
+ When clicking on a anchor (<a href="...">)
+ */
 $(function () {
-    $('.scroll-to a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('.scroll-to a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 
-        	var ofs = $('.navigation').height();
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top - ofs
-                }, 1200 );
-                return false;
-            }
-        }
-    });
+      var ofs = $('.navigation').height();
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - ofs
+        }, 1200);
+        return false;
+      }
+    }
+  });
 });
 
 
-$( document ).ready(function(){
-	$(window).on('scroll', _.throttle(stickNav, 150));
+$(document).ready(function () {
+  $(window).on('scroll', _.throttle(stickNav, 150));
 });
 
-function stickNav(){
-	if( $(window).scrollTop() > 245 ){
-		$('.navie').addClass('x--sticky');
-	} else {
-		$('.navie').removeClass('x--sticky');
-	}
+function stickNav() {
+  if ($(window).scrollTop() > 245) {
+    $('.navie').addClass('x--sticky');
+  } else {
+    $('.navie').removeClass('x--sticky');
+  }
 }
 
 
-
 /* Pinto container */
-$( document ).ready(function(){
+$(document).ready(function () {
 
-	Macy.init({
-		container: '#mason',
-		trueOrder: false,
-		columns: 5,
-		waitForImages: false,
-		margin: 20
-	});
+  Macy.init({
+    container: '#mason',
+    trueOrder: false,
+    columns: 5,
+    waitForImages: false,
+    margin: 20
+  });
 
-	// setTimeout(function(){
-	// 	$('#mason div').velocity('slideUpIn', {
-	// 		duration: 1300,
-	// 		stagger: 500
-	// 	}, 1000 );
-	// });
+  // setTimeout(function(){
+  // 	$('#mason div').velocity('slideUpIn', {
+  // 		duration: 1300,
+  // 		stagger: 500
+  // 	}, 1000 );
+  // });
 
-	setTimeout(function(){
-		$('#mason > div').velocity('slideUpIn', {
-			duration: 1000,
-			stagger: 400
-		}, 1000 );
-	});
+  setTimeout(function () {
+    $('#mason > div').velocity('slideUpIn', {
+      duration: 1000,
+      stagger: 400
+    }, 1000);
+  });
 
 });
 
